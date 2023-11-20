f3d8726bd9d077951cfb701e41b575dc1936bd36
rostikfebruary <lyutyirostik@gmail.com>
2023-11-20 21:19:56 +0200

    hw1

---
+++ HW1 JS/JSh1.js
@@ -0,0 +1,34 @@
+
+//strings
+
+let str1='hello';
+let str2='owu';
+let str3='com';
+let str4='ua';
+console.log(str1, str2, str3, str4);
+
+//numbers
+
+let n1=1;
+let n2=10;
+let n3=-999;
+let n4=123;
+let n5=3.14;
+let n6=2.7;
+let n7=16;
+console.log(n1, n2, n3, n4, n5, n6, n7);
+
+//boolean
+let b1=true;
+let b2=false;
+console.log(b1, b2);
+
+
+
+
+let firstName="Rostik";
+let middleName="Romanovich";
+let lastName="Liutyi";
+
+let myFullName=firstName +' ' + middleName +' ' + lastName;
+console.log(myFullName);
